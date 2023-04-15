b5df8513efd50c53cccfcf16778e41169d783041
Ahsan Ghalib <ahsan@hatechnologies.net>
2023-04-15 13:55:20 +0500

    Added CategoryEnum

--- app/Enums/CategoryEnum.php
+++ app/Enums/CategoryEnum.php
@@ -2,7 +2,23 @@
 
 namespace App\Enums;
 
-enum CategoryEnum
-{
+use App\Traits\ArrayableEnum;
 
+enum CategoryEnum: string
+{
+    use ArrayableEnum;
+    case Business = 'Business';
+    case Entertainment = 'Entertainment';
+    case Health = 'Health';
+    case Politics = 'Politics';
+    case Science = 'Science';
+    case Sports = 'Sports';
+    case Technology = 'Technology';
+    case World = 'World';
+    case News = 'News';
+    case Sport = 'Sport';
+    case Culture = 'Culture';
+    case Lifestyle = 'Lifestyle';
+    case Opinion = 'Opinion';
+    case Environment = 'Environment';
 }
